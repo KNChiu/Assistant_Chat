1892883692b35a8c86351fd7b83f0b60f2288212
KNChiu <y9760210@gmail.com>
2024-06-13 11:42:04 +0800

    Add: Use dropdown to change model #5

--- app.py
+++ app.py
@@ -22,10 +22,19 @@ with gr.Blocks() as demo:
         chatbot = gr.Chatbot(height=300)
         history = gr.State(value=[])
 
-    with gr.Row(): 
-        system_prompt_radio = gr.Radio(
-                ["Code", "問答", "總結", "翻譯", "搜尋", "無"], show_label=False
-        )
+    with gr.Row():
+        with gr.Column(scale=6):
+            system_prompt_radio = gr.Radio(
+                choices=["Code", "問答", "總結", "翻譯", "搜尋", "無"],
+                show_label=False
+            )
+            
+        with gr.Column(scale=1):
+            model_dropdown = gr.Dropdown(
+                choices=['gpt-3.5-turbo', 'gpt-4o'],
+                value='gpt-3.5-turbo',
+                show_label=False
+            )
 
     with gr.Row():
         with gr.Accordion("System Prompt", open=False) as accordion:
@@ -50,18 +59,31 @@ with gr.Blocks() as demo:
     llmgenerate = LLMGenerate(model)
 
     def task_choice(system_prompt_radio, system_prompt_input, chatbot):
-        if system_prompt_radio == "Google 搜尋":
+        if system_prompt_radio == "搜尋":
             chatbot[-1][1], _ = searchqa.search_QA(chatbot[-1][0])
             submit_button = "Send (search)"
         else:
             chatbot, submit_button = llmgenerate.chat_QA(system_prompt_input, chatbot)
 
         return chatbot, submit_button
+    
+    def change_model(model_dropdown):
+        global model, searchqa, llmgenerate
+        model = model_dropdown
+        searchqa = SearchQA(model)
+        llmgenerate = LLMGenerate(model)
+
 
     system_prompt_radio.change(
         fn=userinterface.change_prompt, 
         inputs=system_prompt_radio, 
         outputs=system_prompt_input
+    )
+
+
+    model_dropdown.change(
+        fn=change_model, 
+        inputs=model_dropdown,
     )
 
     submit_button.click(
