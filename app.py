dd2483ee5279fa6de1c21bd2f703606562f984cb
KNChiu <y9760210@gmail.com>
2024-06-26 09:56:15 +0800

    Fix: Prompt calling #1

--- app.py
+++ app.py
@@ -51,8 +51,15 @@ with gr.Blocks() as demo:
         clear_button = gr.Button("Clear")
 
     num_tokens = 0
-
-    userinterface = UserInterface()
+    prompt_setting = {
+        "Code": "你是一個生活在台灣的資深軟體工程師，使用 python 為主的程式語言，請根據提問生成合適的程式碼，並使用繁體中文條列說明功能",
+        "問答": "你是一位人工智慧領域的專家，請專業並有邏輯的使用繁體中文回答問題",
+        "總結": "你是一位重點統整的專家，請依據輸入的內容統整成簡短且有意義的文字，使用繁體中文回答",
+        "翻譯": "依據輸入的文字，判斷是英文還是中文，如果輸入是英文翻譯成通順的繁體中文，如果輸入是中文則翻譯成通順的英文",
+        "搜尋": "依據 Google 搜尋結果統整資訊，並使用繁體中文簡短回答"
+    }
+    
+    userinterface = UserInterface(prompt_setting)
 
     searchqa = SearchQA(model)
     llmgenerate = LLMGenerate(model)
--- utils/user_interface.py
+++ utils/user_interface.py
@@ -1,32 +1,25 @@
 class UserInterface:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, prompt_setting) -> None:
+        self.prompt_setting = prompt_setting
 
     def change_prompt(self, choice: str) -> str:
-        """
-        Return different prompt messages based on the user's choice
-
-        Parameters:
-        choice (str): The user's choice
-
-        Returns:
-        str: The corresponding prompt message for the choice, respond in Traditional Chinese by default
-        """
-        if choice == "Code":
-            return "你是一個生活在台灣的資深軟體工程師，使用 python 為主的程式語言，請根據提問生成合適的程式碼，並使用繁體中文條列說明功能"
-        elif choice == "問答":
-            return "你是一位人工智慧領域的專家，請專業並有邏輯的使用繁體中文回答問題"
-        elif choice == "總結":
-            return "你是一位重點統整的專家，請依據輸入的內容統整成簡短且有意義的文字，使用繁體中文回答"
-        elif choice == "翻譯":
-            return "依據輸入的文字，判斷是英文還是中文，如果輸入是英文翻譯成通順的中文，如果輸入是中文則翻譯成通順的英文"
-        elif choice == "搜尋":
-            return "依據 Google 搜尋結果統整資訊，並使用繁體中文簡短回答"
-        else:
-            return "使用繁體中文回應以下問題"
+        return self.prompt_setting.get(choice, "使用繁體中文回應以下問題")
 
     def user_set(self, message: str, history: list) -> tuple:
         return "", history + [[message, ""]]
 
     def clear_chat(self) -> tuple:
         return "", "Send (0)"
+
+
+if __name__ == "__main__":
+    prompt_setting = {
+        "Code": "你是一個生活在台灣的資深軟體工程師，使用 python 為主的程式語言，請根據提問生成合適的程式碼，並使用繁體中文條列說明功能",
+        "問答": "你是一位人工智慧領域的專家，請專業並有邏輯的使用繁體中文回答問題",
+        "總結": "你是一位重點統整的專家，請依據輸入的內容統整成簡短且有意義的文字，使用繁體中文回答",
+        "翻譯": "依據輸入的文字，判斷是英文還是繁體中文，如果輸入是英文翻譯成通順的中文，如果輸入是中文則翻譯成通順的英文",
+        "搜尋": "依據 Google 搜尋結果統整資訊，並使用繁體中文簡短回答",
+    }
+
+    userinterface = UserInterface(prompt_setting)
+    print(userinterface.change_prompt(choice="Code"))
